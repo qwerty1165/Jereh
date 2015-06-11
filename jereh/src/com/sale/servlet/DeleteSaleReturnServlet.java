76c535566652be616e6f97342e65fcd1d9288a59
qwerty1165 <1165141555@qq.com>
2015-06-11 10:06:49 +0800

    slz

---
+++ jereh/src/com/sale/servlet/DeleteSaleReturnServlet.java
@@ -0,0 +1,41 @@
+package com.sale.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sale.service.SaleReturnService;
+import com.sale.service.impl.SaleReturnServiceImpl;
+
+public class DeleteSaleReturnServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public DeleteSaleReturnServlet() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response);
+	}
+
+	private SaleReturnService saleReturnService = new SaleReturnServiceImpl();
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/json; charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		String code = request.getParameter("code");
+		saleReturnService.deleteSaleReturn(code);
+	}
+}
---
+++ jereh/src/com/sale/servlet/OutputExcelSaleReturnServlet.java
@@ -0,0 +1,43 @@
+package com.sale.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OutputExcelSaleReturnServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public OutputExcelSaleReturnServlet() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		String pageNo = request.getParameter("page");
+		String pageSize = request.getParameter("rows");
+		if (pageNo == null || pageNo.equals("")) {
+			pageNo = "1";
+		}
+		if (pageSize == null || pageSize.equals("")) {
+			pageSize = "10";
+		}
+
+		super.doPost(request, response);
+	}
+}
